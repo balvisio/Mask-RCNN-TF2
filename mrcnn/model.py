cfa63b12275aef13c36f6b23840e665ed966fd57
Bruno Alvisio <bruno.alvisio@gmail.com>
2022-11-21 04:39:42 -0600

    Import tf.keras instead of keras

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -17,11 +17,10 @@ from collections import OrderedDict
 import multiprocessing
 import numpy as np
 import tensorflow as tf
-import keras
-import keras.backend as K
-import keras.layers as KL
-import keras.engine as KE
-import keras.models as KM
+from tensorflow import keras
+import tensorflow.keras.backend as K
+import tensorflow.keras.layers as KL
+import tensorflow.keras.models as KM
 
 from mrcnn import utils
 
